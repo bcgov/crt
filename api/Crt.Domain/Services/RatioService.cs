efb215fb667e09fb7581d5130ff382d74f806f3a
bcgov-brwang <bcgov.brwang@gmail.com>
2024-07-29 22:13:42 -0700

    bugfix/1.0.6: to fix pipeline build error

--- api/Crt.Domain/Services/RatioService.cs
+++ api/Crt.Domain/Services/RatioService.cs
@@ -341,6 +341,11 @@ namespace Crt.Domain.Services
 
                 if (percentInPolygon > 0)
                 {
+                    var existingRatioRecord = _validator.CodeLookup
+                            .Where(x => x.CodeSet == CodeSet.EconomicRegion && x.CodeValueText == polygon.Number)
+                            .FirstOrDefault();
+                    if (existingRatioRecord == null) continue;
+
                     //generate the new ratio
                     var newRatio = new RatioCreateDto
                     {
